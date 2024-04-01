767ee79a0a2030b2d034a61eb4b772dac3c56f3a
vUh0nGqUaNg <vuhongquangit@gmail.com>
2024-04-02 02:11:41 +0700

    VHQuang | some improment

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
     public static void main (String[] args){
         int again = 1;
         while (again == 1){
-            System.out.print("How many games do you want to play: ");
+            System.out.print("How many tickets do you want to buy: ");
             int numberLoop = scanner.nextInt();
             loop.isLoop(numberLoop);
-            System.out.println("The number of games won any money: "+loop.getTotalWon());
-            System.out.println("The number of games won all 6 numbers: "+loop.getTotalJackpot());
+            System.out.println("The number of ticket won any money: "+loop.getTotalWon());
+            System.out.println("The number of ticket won all 6 numbers: "+loop.getTotalJackpot());
             System.out.println("Total money won: $"+loop.getTotalPrize());
             System.out.println("Total ticket cost: $"+(numberLoop*4));
             System.out.println("Profit/Loss: $"+(loop.getTotalPrize()-(numberLoop*4)));
-            System.out.print("Do you want get information about any specific match ? (Input match number from 1 to "+numberLoop+" OR 0 to exit): ");
+            System.out.print("Do you want get information about any specific ticket ? (Input ticket number from 1 to "+numberLoop+" OR 0 to exit): ");
             int specificMatch = scanner.nextInt() - 1;// reduce 1 to match index
             loop.getInfoMatch(specificMatch, numberLoop);
             System.out.print("Do you want to play again ? (1.Yes\t2.No): ");
